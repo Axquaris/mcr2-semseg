57246b9b163e94c857f742713043e6f981985b0a
axquaris <dominykas@berkeley.edu>
2020-12-12 13:37:49 -0800

    debugging val

--- models/cnn.py
+++ models/cnn.py
@@ -5,6 +5,7 @@ from mcr2_loss import MaximalCodingRateReduction
 import pytorch_lightning as pl
 from models.classifiers import *
 from models.resnet import ResNet18, ResNet10MNIST
+from easydict import EasyDict
 
 
 def to_wandb_im(x):
@@ -51,7 +52,7 @@ class CNN(pl.LightningModule):
         if self.loss == 'mcr2':
             self.criterion = MaximalCodingRateReduction(num_classes)
             self.classifier = None
-            self._reset_agg()
+            self.reset_agg()
         elif self.loss == 'ce' and self.task == 'classify':
             self.criterion = nn.CrossEntropyLoss()
             self.classifier = nn.Linear(feat_dim, num_classes)
@@ -60,12 +61,7 @@ class CNN(pl.LightningModule):
             self.classifier = nn.Conv2d(feat_dim, num_classes, kernel_size=1, padding=0)
         self.accuracy = pl.metrics.Accuracy()
 
-    def _agg(self, mcr_ret):
-        self.__ZtPiZ += mcr_ret.ZtPiZ
-        self.__Z_mean += mcr_ret.Z_mean
-        self.__num_batches += 1
-
-    def _reset_agg(self):
+    def reset_agg(self):
         self.__ZtPiZ = torch.zeros(self.num_classes, self.feat_dim, self.feat_dim).cuda()
         self.__Z_mean = torch.zeros(self.feat_dim).cuda()
         self.__num_batches = 0.
@@ -78,20 +74,17 @@ class CNN(pl.LightningModule):
     def Z_mean(self):
         return self.__Z_mean / self.__num_batches
 
-    def forward(self, x):
-        z = self.encoder(x)
-        if self.loss == 'mcr2':
-            # Normalize to unit length
-            feats = z / torch.norm(z, dim=1, keepdim=True)
-        return feats
+    def forward(self, x, labels=None, log=False, log_img=False):
+        """
 
-    def training_step(self, batch, batch_idx):
-        # x      shape (batch_size, C,     H, W)
-        # feats  shape (batch_size, feat_dim, H, W)
-        # labels shape (batch_size, H, W)
-        x, labels = batch
-        labels = torch.squeeze(labels)
-        feats = self(x)
+        :param x:
+        :param labels:
+        :param log: str with logging prefix
+        :param log_img: bool
+        :return:
+        """
+        metrics = EasyDict()
+        feats = self.encoder(x)
 
         if self.task == 'classify':
             labels, _ = labels.view(labels.shape[0], -1).max(-1)
@@ -99,6 +92,8 @@ class CNN(pl.LightningModule):
                 feats = feats.view(*feats.shape[:-2], -1).mean(-1)
 
         if self.loss == 'mcr2':
+            feats = feats / torch.norm(feats, dim=1, keepdim=True)
+
             Z = feats.transpose(0, 1).reshape(feats.shape[1], -1).T
             Y = labels.view(-1)
             mcr_ret = self.criterion(Z, Y)
@@ -107,86 +102,91 @@ class CNN(pl.LightningModule):
             self.__num_batches += 1
 
             loss = mcr_ret.loss
-            preds = self.classifier(Z).view(labels.shape) if self.classifier else None
+            preds = self.classifier(Z).view(x.shape) if self.classifier else None
 
-            self.log('train_discrim_loss', mcr_ret.discrim_loss)
-            self.log('train_compress_loss', mcr_ret.compress_loss)
-            self.log('train_ZtPiZ_mean', torch.mean(mcr_ret.ZtPiZ))
-            self.log('train_Z_mean', torch.mean(mcr_ret.Z_mean))
+            metrics.update(
+                discrim_loss=mcr_ret.discrim_loss,
+                compress_loss=mcr_ret.compress_loss,
+                ZtPiZ_mean=torch.mean(mcr_ret.ZtPiZ),
+                Z_mean=torch.mean(mcr_ret.Z_mean),
+            )
         else:
             logits = self.classifier(feats)
-            loss = self.criterion(logits, labels)
+            loss = self.criterion(logits, labels) if labels is not None else None
             preds = logits.argmax(dim=1)
 
-        self.log('train_loss', loss, on_step=True, prog_bar=True)
-        if preds is not None:
-            self.log('train_acc', self.accuracy(preds, labels), on_step=True, prog_bar=True)
-        if batch_idx == 100:
-            if self.task == 'semseg':
-                class_labels = {0: 'background'}
-                class_labels.update({v+1: str(v) for v in range(10)})
-                masks = {"ground_truth": {"mask_data": to_wandb_im(labels[0]), "class_labels": class_labels}}
-                if preds is not None:
-                    masks["predictions"] = {"mask_data": to_wandb_im(preds[0]), "class_labels": class_labels}
-                img = wandb.Image(to_wandb_im(x[0]), masks=masks)
-                self.logger.experiment.log({'train_img': [img]}, commit=False)
-        return loss
+        metrics.loss = loss
+        if preds is not None and labels is not None:
+            metrics.acc = self.accuracy(preds, labels)
+
+        # All logging ops
+        if log:
+            for k in metrics:
+                m = metrics[k]
+                if k in {'loss' or 'acc'}:
+                    self.log(f'{log}_{k}', m, on_step=True, prog_bar=True)
+                else:
+                    self.log(f'{log}_{k}', m)
+
+            if log_img:
+                if self.task == 'semseg':
+                    imgs = []
+                    for i in range(10):
+                        masks = {}
+                        if labels is not None:
+                            masks["ground_truth"] = {"mask_data": to_wandb_im(labels[i]), "class_labels": self.class_labels}
+                        if preds is not None:
+                            masks["predictions"] = {"mask_data": to_wandb_im(preds[i]), "class_labels": self.class_labels}
+                        imgs.append(wandb.Image(to_wandb_im(x[i]), masks=masks))
+                    self.logger.experiment.log({f'{log}_img': imgs}, commit=False)
+
+        return EasyDict(
+            loss=loss,
+            feats=feats.detach(),
+            preds=preds,
+            metrics=metrics,
+        )
+
+    def training_step(self, batch, batch_idx):
+        # x      shape (batch_size, C,     H, W)
+        # feats  shape (batch_size, feat_dim, H, W)
+        # labels shape (batch_size, H, W)
+        x, labels = batch
+        labels = torch.squeeze(labels)
+        ret = self(x, labels, log='train', log_img=(batch_idx == 0))
+
+        return ret.loss
 
     def training_epoch_end(self, outputs):
         if self.loss == 'mcr2':
             self.classifier = FastNearestSubspace(self.ZtPiZ, self.Z_mean,
                                                   num_classes=self.num_classes,
                                                   n_components=self.feat_dim // self.num_classes)
-            self._reset_agg()
+            self.reset_agg()
 
-    # def validation_step(self, batch, batch_idx):
-    #     # x      shape (batch_size, C,     H, W)
-    #     # feats  shape (batch_size, feat_dim, H, W)
-    #     # labels shape (batch_size, H, W)
-    #     x, labels = batch
-    #     labels = torch.squeeze(labels)
-    #     feats = self(x)
-    #
-    #     if self.task == 'classify':
-    #         labels, _ = labels.view(labels.shape[0], -1).max(-1)
-    #         if self.encode_arch == 'cnn':
-    #             feats = feats.view(*feats.shape[:-2], -1).mean(-1)
-    #
-    #     if self.loss == 'mcr2':
-    #         Z = feats.transpose(0, 1).view(feats.shape[1], -1).T
-    #         Y = labels.view(-1)
-    #         mcr_ret = self.criterion(Z, Y)
-    #         self._agg(mcr_ret)
-    #
-    #         loss = mcr_ret.loss
-    #         preds = self.classifier(Z).view(labels.shape) if self.classifier else None
-    #
-    #         self.log('val_discrim_loss', mcr_ret.discrim_loss)
-    #         self.log('val_compress_loss', mcr_ret.compress_loss)
-    #         self.log('val_ZtPiZ_mean', torch.mean(mcr_ret.ZtPiZ))
-    #         self.log('val_Z_mean', torch.mean(mcr_ret.Z_mean))
-    #     else:
-    #         logits = self.classifier(feats)
-    #         loss = self.criterion(logits, labels)
-    #         preds = logits.argmax(dim=1)
-    #
-    #     self.log('val_loss', loss, on_step=True, prog_bar=True)
-    #     if preds is not None:
-    #         self.log('val_acc', self.accuracy(preds, labels), on_step=True, prog_bar=True)
-    #     if batch_idx == 100:
-    #         if self.task == 'semseg':
-    #             class_labels = {0: 'background'}
-    #             class_labels.update({v + 1: str(v) for v in range(10)})
-    #             masks = {"ground_truth": {"mask_data": to_wandb_im(labels[0]), "class_labels": class_labels}}
-    #             if preds is not None:
-    #                 masks["predictions"] = {"mask_data": to_wandb_im(preds[0]), "class_labels": class_labels}
-    #             img = wandb.Image(to_wandb_im(x[0]), masks=masks)
-    #             self.logger.experiment.log({'val_img': [img]}, commit=False)
-    #     return loss
-    #
-    # def validation_epoch_end(self, outputs):
-    #     ...
-        # self.log('val_acc_epoch', self.accuracy.compute())
+    # TODO: might need to re-compute classifier over validation epoch
+    # TODO: --OR-- compute classifier for each validation batch?
+    # TODO: try other classifiers of Z
+    # def on_validation_epoch_end(self):
+    #     self.classifier = None
+    #     self.reset_agg()
+
+    def validation_step(self, batch, batch_idx):
+        x, labels = batch
+        labels = torch.squeeze(labels)
+        ret = self(x, labels, log='val', log_img=(batch_idx % 10 == 0))
+
+        # TODO: agg over epoch
+        # TODO: confusion matrix
+        return ret.metrics
+
+    def validation_epoch_end(self, outputs):
+        # self.classifier = FastNearestSubspace(self.ZtPiZ, self.Z_mean,
+        #                                       num_classes=self.num_classes,
+        #                                       n_components=self.feat_dim // self.num_classes)
+        # self.reset_agg()
+
+        self.log('val_acc_epoch', self.accuracy.compute())
 
     def configure_optimizers(self):
         return torch.optim.SGD(self.parameters(), lr=self.lr)
