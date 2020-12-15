de7370db4c5b86e834ee3e7c83d81530e08ab4a8
Nathan Miller <nathan_miller23@berkeley.edu>
2020-12-15 15:47:59 -0800

    added background classifier

--- train_supervised.py
+++ train_supervised.py
@@ -18,18 +18,20 @@ def main():
     parser.add_argument('--es', type=int, default=100, help='num epochs (default: 10)')
     parser.add_argument('--bs', type=int, default=1000, help='batch size (default: 1000)')
 
-    parser.add_argument('--arch', type=str, choices=['cnn', 'resnet10', 'resnet18', 'unet'], default='cnn', help='What encoder to use')
+    parser.add_argument('--arch', type=str, choices=['cnn', 'resnet10', 'resnet18', 'unet', 'unet_bg', 'unet_bg_separate'], default='cnn', help='What encoder to use')
     parser.add_argument('--task', type=str, default='classify', help='semseg or classify')
     parser.add_argument('--loss', type=str, choices=['mcr2', 'ce', 'hybrid'], default='mcr2', help='')
     parser.add_argument('--eps', type=float, default=.5, help='mcr2 eps param')
     parser.add_argument('-fd', '--feat_dim', type=int, default=128, help='dimension of features (default: 128)')
     parser.add_argument('--lr', type=int, default=1e-3, help='learning rate')
+    parser.add_argument('--bg_weight', type=float, default=1.0, help="Relative weight of bg loss to mcr2 loss")
 
     parser.add_argument('--debug', action='store_true', default=False)
 
     args = EasyDict(vars(parser.parse_args()))
     for k in args.keys():
-        args[k] = args[k].tolowercase()
+        if type(args[k]) == str:
+            args[k] = args[k].lower()
 
     torch.backends.cudnn.benchmark = True
 
@@ -50,6 +52,7 @@ def main():
     dataloader_args = EasyDict(batch_size=args.bs, shuffle=False, num_workers=0 if args.debug else 4)
     train_dataloader = DataLoader(train_dataset, **dataloader_args)
     val_dataloader = DataLoader(val_dataset, **dataloader_args)
+    bg_encoder = None
 
     if args.arch == 'cnn':
         encoder = cnn.get_mnist_semseg(in_c=im_channels, feat_dim=args.feat_dim)
@@ -59,9 +62,16 @@ def main():
         encoder = resnet.get_mnist_resnet(in_c=im_channels, feat_dim=args.feat_dim, depth="18")
     elif args.arch == 'unet':
         encoder = unet.UNet(n_channels=im_channels, feat_dim=args.feat_dim)
+    elif args.arch == 'unet_bg':
+        encoder = unet.UNet(n_channels=im_channels, feat_dim=args.feat_dim + 2)
+        args['loss'] = 'mcr2_bg'
+    elif args.arch == 'unet_bg_separate':
+        encoder = unet.UNet(n_channels=im_channels, feat_dim=args.feat_dim)
+        bg_encoder = unet.UNet(n_channels=im_channels, feat_dim=2)
+        args['loss'] = 'mcr2_bg'
     else:
         raise NotImplementedError(args.model)
-    model = MainModel(encoder, 11, **args, class_labels=train_dataset.class_labels)
+    model = MainModel(encoder, 11, **args, class_labels=train_dataset.class_labels, bg_encoder=bg_encoder)
 
     logger = WandbLogger(project='mcr2-semseg', config=args, name=args.name)
     trainer = pl.Trainer(gpus=1, max_epochs=args.es, logger=logger, auto_select_gpus=True, log_every_n_steps=10)
